47fc5077c871a9d8f5d4b655312aa5a5a90c2e5d
sinchang <sinchang@foxmail.com>
2017-03-20 16:51:40 +0800

    add Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import App from 'components/App'
 import bulma from 'bulma'
 import Toasted from 'vue-toasted'
 import SocialSharing from 'vue-social-sharing'
+import ga from 'vue-ga'
+
+ga(collect => {
+  window.onhashchange = () => {
+    collect(location.pathname + location.hash)
+  }
+}, 'UA-41610509-8')
 
 Vue.use(SocialSharing)
 Vue.use(Toasted)
