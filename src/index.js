cea532dac5ad2d1f99a528f7d00ac0ba12ac3ad5
sinchang <sinchangwen@gmail.com>
2017-03-24 10:38:24 +0800

    :package:

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import App from 'components/App'
 import bulma from 'bulma'
 import Toasted from 'vue-toasted'
 import SocialSharing from 'vue-social-sharing'
-import ga from 'vue-ga'
+import VueAnalytics from 'vue-analytics'
 
-ga(collect => {
-  window.onhashchange = () => {
-    collect(location.pathname + location.hash)
-  }
-}, 'UA-41610509-8')
-
+Vue.use(VueAnalytics, {
+  id: 'UA-41610509-8'
+})
 Vue.use(SocialSharing)
 Vue.use(Toasted)
 
