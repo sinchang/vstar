7cb3f72a673270758e81c0ebe10868536a5b478c
sinchang <sinchangwen@gmail.com>
2018-11-23 11:20:03 +0800

    Remove Fundebug

--- src/index.js
+++ src/index.js
@@ -6,18 +6,6 @@ import SocialSharing from 'vue-social-sharing'
 Vue.use(SocialSharing)
 Vue.use(Toasted)
 
-// Fundebug 错误监控
-Vue.config.errorHandler = function (err, vm) {
-  var componentName = Vue.util.formatComponentName(vm),
-    propsData = vm.$options.propsData
-  fundebug.notifyError(err, {
-    metaData: {
-      componentName: componentName,
-      propsData: propsData
-    }
-  })
-}
-
 new Vue({
   el: '#app',
   render: h => h(App)
